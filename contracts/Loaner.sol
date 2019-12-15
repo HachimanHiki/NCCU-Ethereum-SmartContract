17a1d1bcb17458a50a6318e0ec26fc5968959be6
HachimanHiki <mh512158475@gmail.com>
2019-12-16 01:37:11 +0800

    optimization

--- contracts/Loaner.sol
+++ contracts/Loaner.sol
@@ -7,24 +7,24 @@ contract Loaner is Base{
     event GuarantyETH (address indexed loaner, uint256 loanValue);
 
     function depositETHAndGuaranty(uint256 _oneEtherExchangeTokenRate) public payable{
-        uint256 numOfTokenSell = _oneEtherExchangeTokenRate * msg.value;
-        require( erc20Token.balanceOf(address(this)) >= numOfTokenSell );
-
+        _guarantyETH(_oneEtherExchangeTokenRate, msg.value);
         etherBalance[msg.sender] = etherBalance[msg.sender].add(msg.value);
-        lockedEther[msg.sender] = lockedEther[msg.sender].add(msg.value);
-        borrowEther[msg.sender] = borrowEther[msg.sender].add(msg.value);
-        borrowInfo[msg.sender].initBorrowTime = now;
-        borrowInfo[msg.sender].initBorrowRate = _oneEtherExchangeTokenRate;
 
         emit GuarantyETH(msg.sender, msg.value);
         //_buyToken.transfer(0xC5E1b1c0BBb1587c7595Ec6deAe8e6BE6bBfbdF4, numOfTokenSell);
     }
 
     function guarantyETH(uint256 _oneEtherExchangeTokenRate, uint256 _guarantyValue) public{
+        require( getUnlockEtherBalance(msg.sender) >= _guarantyValue);
+        _guarantyETH(_oneEtherExchangeTokenRate, _guarantyValue);
+
+        emit GuarantyETH(msg.sender, _guarantyValue);
+    }
+
+    function _guarantyETH(uint256 _oneEtherExchangeTokenRate, uint256 _guarantyValue) public{
         uint256 numOfTokenSell = _oneEtherExchangeTokenRate * _guarantyValue;
         require( erc20Token.balanceOf(address(this)) >= numOfTokenSell );
 
-        require( getUnlockEtherBalance(msg.sender) >= _guarantyValue);
         lockedEther[msg.sender] = lockedEther[msg.sender].add(_guarantyValue);
         borrowEther[msg.sender] = borrowEther[msg.sender].add(_guarantyValue);
         borrowInfo[msg.sender].initBorrowTime = now;
