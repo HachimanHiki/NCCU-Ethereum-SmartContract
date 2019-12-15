9ae0d22aef19531164701ee220c791da36c012b3
HachimanHiki <mh512158475@gmail.com>
2019-12-15 16:16:22 +0800

    update

--- contracts/Loaner.sol
+++ contracts/Loaner.sol
@@ -17,6 +17,17 @@ contract Loaner is Base{
         //_buyToken.transfer(0xC5E1b1c0BBb1587c7595Ec6deAe8e6BE6bBfbdF4, numOfTokenSell);
     }
 
+    function guarantyETH(uint256 _oneEtherExchangeTokenRate, uint256 _guarantyValue) public{
+        uint256 numOfTokenSell = _oneEtherExchangeTokenRate * _guarantyValue;
+        require( erc20Token.balanceOf(address(this)) >= numOfTokenSell );
+
+        require( getUnlockEtherBalance(msg.sender) >= _guarantyValue);
+        lockedEther[msg.sender] = lockedEther[msg.sender].add(_guarantyValue);
+        borrowEther[msg.sender] = borrowEther[msg.sender].add(_guarantyValue);
+
+        emit GuarantyETH(msg.sender, _guarantyValue);
+    }
+
     event SellAllETH(address indexed loaner);
     event SellETH (address indexed loaner, uint256 sellValue);
 
